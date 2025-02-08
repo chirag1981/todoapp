de3fa1487365ad2302de9e550c93ffeb71b3f056
chiragvaidya <cvaidya2017@gmail.com>
2025-02-08 10:00:56 +0530

    init app

--- app.py
+++ app.py
@@ -60,4 +60,6 @@ def update(id):
     
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Default to 10000
+    app.run(host="0.0.0.0", port=port)
+    
